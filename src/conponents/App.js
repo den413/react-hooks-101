2e9c2e678cbfa7e2fde88bd1d6ee02eb45fc5901
den413 <rsbyg390@yahoo.co.jp>
2023-06-12 21:46:06 +0900

    implement evnet creation

--- src/conponents/App.js
+++ src/conponents/App.js
@@ -1,6 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import reducer from "../reducers";
+import { useReducer, useState } from "react";
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const addEvent = (e) => {
+    e.preventDefault();
+
+    dispatch({
+      type: "CREATE_EVENT",
+      title,
+      body,
+    });
+    setTitle("");
+    setBody("");
+  };
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
@@ -14,6 +32,8 @@ const App = () => {
             className="form-control"
             placeholder=""
             aria-describedby="helpId"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
 
@@ -26,10 +46,12 @@ const App = () => {
             className="form-control"
             placeholder=""
             aria-describedby="helpId"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
           />
         </div>
 
-        <button type="button" className="btn btn-primary">
+        <button type="button" className="btn btn-primary" onClick={addEvent}>
           イベントを作成する
         </button>
         <button type="button" className="btn btn-danger">
